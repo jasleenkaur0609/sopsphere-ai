0599ec0966d662327ec0edaa8a4e20d83467ca02
Jasleen Kaur <jasleensejal2003@gmail.com>
2026-08-08 14:23:45 +0530

    Updated teh files fr authenticaation and register process

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -3,6 +3,10 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home/Home";
 import Login from "../pages/Authentication/Login";
 import Register from "../pages/Authentication/Register";
+import VerifyEmailOTP from "../pages/Authentication/VerifyEmailOTP";
+// import CompleteProfile from "../pages/Authentication/CompleteProfile";
+// import VerifyMobileOTP from "../pages/Authentication/VerifyMobileOTP";
+
 // import ForgotPassword from "../pages/Authentication/ForgotPassword";
 // import Dashboard from "../pages/Dashboard/Dashboard";
 
@@ -16,9 +20,24 @@ function AppRoutes() {
 
       <Route path="/login" element={<Login />} />
 
-      {/* Uncomment when ready */}
-
       <Route path="/register" element={<Register />} />
+
+      <Route
+        path="/verify-email"
+        element={<VerifyEmailOTP />}
+      />
+
+      {/* <Route
+        path="/complete-profile"
+        element={<CompleteProfile />}
+      />
+
+      <Route
+        path="/verify-mobile"
+        element={<VerifyMobileOTP />} 
+      />*/}
+
+      {/* Uncomment when ready */}
 
       {/* <Route path="/forgot-password" element={<ForgotPassword />} /> */}
 
@@ -28,7 +47,10 @@ function AppRoutes() {
 
       {/* Fallback */}
 
-      <Route path="*" element={<Navigate to="/" replace />} />
+      <Route
+        path="*"
+        element={<Navigate to="/" replace />}
+      />
 
     </Routes>
   );
